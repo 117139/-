7ee7378c33c4d0cd71a0839a485389a716f743d5
wenxin <1171390035@qq.com>
2021-02-24 17:35:19 +0800

    2020-2-24

--- js/public_su.js
+++ js/public_su.js
@@ -8,3 +8,25 @@ $(window).resize(function() { //当浏览器大小变化时
 	}
 
 });
+
+/**
+ * 页面传参
+ * @returns {Object}
+ */
+function getNowCanshu() {
+  var aa = window.location.href;//返回当前页面的url
+  var urlinfo = aa.replace("#", " ");
+  var canshu = function() {
+    var ind = urlinfo.indexOf('?');//返回某个指定的字符串值在字符串中首次出现的位置
+    var cs = urlinfo.substr(ind + 1);
+    var tempobj = new Object();
+    var csarr = cs.split("&");
+    jQuery.each(csarr, function(i, v) {
+      var temparr = v.split("=");
+      var objname = temparr[0];
+      tempobj[objname] = temparr[1];
+    });
+    return tempobj;
+  }();
+  return canshu;
+}
